70faa3807af94bfdd513b86567aef2565d6d33e8
Yegor Yefimov <egoryefim@gmail.com>
2022-05-12 21:38:21 +0300

    -

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,23 +1,27 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
+
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
 
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
             namespace: 'a',
             path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           }
-        }
+      })
 
+      build.onResolve({ filter: /.*/ }, async (args: any) => { 
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
@@ -30,22 +34,29 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import React from 'react';
-              console.log(React);
-            `,
+            contents: inputCode,
           };
         }
 
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
-        }
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
 };
 
-//started 9
+//started 9 7
