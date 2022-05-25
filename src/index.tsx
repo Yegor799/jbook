b5a2dd76eb19d254e0397cea6837aa3002da516e
Yegor <you@example.com>
2022-05-25 13:05:35 +0300

    small changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,88 +1,33 @@
-import * as esbuild from 'esbuild-wasm';
 import ReactDOM from "react-dom/client";
-import { useState, useEffect, useRef } from "react";
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { useState } from "react";
 import CodeEditor from './components/code-editor';
-
+import Preview from './components/preview';
+import bundler from "./bundler";
 
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+   
   const [input, setInput] = useState('');
+  const [code, setCode] = useState('');  
   
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
-
-  useEffect(() => {
-    startService()
-  }, []);
-  
-
-  const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input),
-      ],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      }
-    });
-    
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-  }
-
-  const html = `
-        <html>
-          <head></head>
-          <body>
-            <div id="root"></div>
-            <script>
-              window.addEventListener('message', (event) => {
-                try {
-                  eval(event.data);
-                } catch (error) {
-                  const root = document.querySelector('#root');
-                  root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + error + '</div>'
-                  console.error(error);
-                }                
-              }, false);
-            </script>
-          </body>
-        </html>
-  `;
+  const onClick = async () => { 
+    const output = await bundler(input);
+    setCode(output);
+  }  
 
   return (
     <div>
       <CodeEditor
         initialValue='const a = 1'
         onChange={(value) => setInput(value)}
-      />
-      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      />      
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="code preview" ref={iframe} sandbox='allow-scripts' srcDoc={html} />
+      <Preview code={code} />
     </div>
   )
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(  
@@ -92,5 +37,5 @@ root.render(
 
 
 
-//started 12
+//started 12 2
 
