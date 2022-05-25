da58e2d2567012986c8923fc7c66d2747ec20599
Yegor <you@example.com>
2022-05-25 13:37:36 +0300

    small changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundler from "./bundler";
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import CodeCell from "./components/code-cell";
 
-
-const App = () => {
-   
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');  
-  
-  const onClick = async () => { 
-    const output = await bundler(input);
-    setCode(output);
-  }  
-
+const App = () => {     
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1'
-        onChange={(value) => setInput(value)}
-      />      
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />      
     </div>
   )
 }
@@ -37,5 +18,5 @@ root.render(
 
 
 
-//started 12 2
+//started 13
 
