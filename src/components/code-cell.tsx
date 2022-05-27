c7dff0d2125d214ca2d540f7264e6a19d3f33d8b
Yegor <you@example.com>
2022-05-27 10:50:29 +0300

    added error handling

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -6,14 +6,16 @@ import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
+  const [error, setError] = useState('');
   const [input, setInput] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setError(output.error);
     }, 1000);
-    
+
     return () => {
       clearTimeout(timer);
     };
@@ -28,7 +30,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} error={error} />
       </div>
     </Resizable>
   );
